b0463f4e8b1fa59592f8927578a9e7efa6be6952
AdemCan <ademfcan@gmail.com>
2026-01-04 23:53:10 -0800

    Implement empty deactivate function to satisfy linting

--- src/extension.ts
+++ src/extension.ts
@@ -736,7 +736,7 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(disposable);
 }
 
-export function deactivate() {}
+export function deactivate() { }
 
 async function getExecutablePath(
   cstManager: CstManager,
